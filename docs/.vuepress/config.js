7a57b6380ebf746d52fb1e833a39b131b4db7ade
7revor <7revor233@gmail.com>
2023-04-18 22:26:42 +0800

    feat: 修改部分代码

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,84 +1,92 @@
 module.exports = {
-  title: '7revor',
-  description: '可爱又迷人的反派角色',
-  base: '/docs/',
+  title: "7revor",
+  description: "可爱又迷人的反派角色",
+  base: "/",
   head: [
-    ['link', {
-      rel: 'icon',
-      href: '/img/favicon.ico'
-    }]
+    [
+      "link",
+      {
+        rel: "icon",
+        href: "/img/favicon.ico",
+      },
+    ],
   ],
   markdown: {
-    lineNumbers: false
+    lineNumbers: false,
   },
   themeConfig: {
-    lastUpdated: 'Last Updated', // string | boolean
-    nav: [{
-      text: 'Home',
-      link: '/'
-    }, {
-      text: 'React',
-      link: '/React/'
-    }, {
-      text: 'Documents',
-      link: '/Notes/'
-    },
+    lastUpdated: "Last Updated", // string | boolean
+    nav: [
       {
-        text: 'MiniApp',
-        link: '/MiniApp/'
-      }, {
-        text: 'Learning',
-        link: '/Learning/'
-      },],
+        text: "Home",
+        link: "/",
+      },
+      {
+        text: "React",
+        link: "/React/",
+      },
+      {
+        text: "Documents",
+        link: "/Notes/",
+      },
+      {
+        text: "MiniApp",
+        link: "/MiniApp/",
+      },
+      {
+        text: "Learning",
+        link: "/Learning/",
+      },
+    ],
     displayAllHeaders: false, //显示所有页面的标题链接
     sidebarDepth: 2,
     sidebar: {
-      '/React/': [
-        ['TabBar', '导航栏组件'],
-        ['Dialog', '动态弹窗'],
-        ['ActionSheet', '动作菜单组件'],
-        ['Animated', 'ReactNative动画指南'],
-        ['HOC', '组件逻辑复用'],
-        ['titleWrapper', '简单实用的高阶组件'],
-        ['closeKeyBoard', 'QAP自动关闭键盘'],
-        ['heightProvider', '组件样式响应键盘高度'],
-        ['listView', 'ListView长列表'],
-        ['cardHoc', '卡片弹窗HOC'],
-        ['context', 'IOS高级功能控制'],
-        ['middleware', '深入理解Redux中间件'],
-        ['QAPerror', 'QAP异常捕获']
+      "/React/": [
+        ["TabBar", "导航栏组件"],
+        ["Dialog", "动态弹窗"],
+        ["ActionSheet", "动作菜单组件"],
+        ["Animated", "ReactNative动画指南"],
+        ["HOC", "组件逻辑复用"],
+        ["titleWrapper", "简单实用的高阶组件"],
+        ["closeKeyBoard", "QAP自动关闭键盘"],
+        ["heightProvider", "组件样式响应键盘高度"],
+        ["listView", "ListView长列表"],
+        ["cardHoc", "卡片弹窗HOC"],
+        ["context", "IOS高级功能控制"],
+        ["middleware", "深入理解Redux中间件"],
+        ["QAPerror", "QAP异常捕获"],
       ],
-      '/Learning/': [
-        ['vuepress', 'VuePress教程'],
-        ['publish', '观察者模式&发布-订阅模式'],
-        ['bind', '双向数据绑定'],
-        ['loop', '事件循环'],
-        ['closure', '执行环境、作用域链与闭包'],
-        ['catch', '全局异常捕获'],
-        ['ast', '抽象语法树'],
-        ['babelPlugin', 'babel插件入门'],
+      "/Learning/": [
+        ["vuepress", "VuePress教程"],
+        ["publish", "观察者模式&发布-订阅模式"],
+        ["bind", "双向数据绑定"],
+        ["loop", "事件循环"],
+        ["closure", "执行环境、作用域链与闭包"],
+        ["catch", "全局异常捕获"],
+        ["ast", "抽象语法树"],
+        ["babelPlugin", "babel插件入门"],
       ],
-      '/Notes/': [
-        ['publish', '发布宝贝'],
-        ['goodsProps', '宝贝属性详解'],
-        ['fieReactUpgrade', 'fie升级React16实录'],
-        ['2019', '毕业这几年']
+      "/Notes/": [
+        ["publish", "发布宝贝"],
+        ["goodsProps", "宝贝属性详解"],
+        ["fieReactUpgrade", "fie升级React16实录"],
+        ["2019", "毕业这几年"],
       ],
-      '/MiniApp/': [
-        ['miniapp', '商家应用知识点梳理'],
-        ['hack', '无侵入式运营对接方案'],
-        ['frame', '全局统一交互实现'],
-        ['navigate', '导航功能扩展'],
-        ['top', '授权接口调用'],
+      "/MiniApp/": [
+        ["miniapp", "商家应用知识点梳理"],
+        ["hack", "无侵入式运营对接方案"],
+        ["frame", "全局统一交互实现"],
+        ["navigate", "导航功能扩展"],
+        ["top", "授权接口调用"],
       ],
       /**
        *  // fallback,确保 fallback 侧边栏被最后定义。VuePress 会按顺序遍历侧边栏配置来寻找匹配的配置。
        */
-      '/': [
-        '', /* / */
+      "/": [
+        "" /* / */,
         /*    'contact', /!* /contact.html *!/
             'about'    /!* /about.html *!/*/
-      ]
-    }
-  }
-}
+      ],
+    },
+  },
+};
--- docs/README.md
+++ docs/README.md
@@ -1,20 +1,21 @@
 ---
 home: true
-heroImage: 
+heroImage:
 actionText: Go →
 actionLink: /React/
 features:
-- title: 产品文档
-  details: 日常工作中的笔记整理以及项目文档。
-- title: 学习笔记
-  details: 工作学习过程中遇到的问题以及思考。
-- title: More
-  details: 期待更多。
---- 
+  - title: 产品文档
+    details: 日常工作中的笔记整理以及项目文档。
+  - title: 学习笔记
+    details: 工作学习过程中遇到的问题以及思考。
+  - title: More
+    details: 期待更多。
+---
+
 <center>
 <div class="footer">
 我们写的不只是代码 / 而是我们读过的书 / 看过的电影 / 听过的音乐 / 走过的路 / 爱过的人
 <br/>
-<a style="font-size:14px;margin-top:12px;display:inline-block;" href="http://www.beian.miit.gov.cn">鲁ICP备19025352号-1</a>
+<!-- <a style="font-size:14px;margin-top:12px;display:inline-block;" href="http://www.beian.miit.gov.cn">鲁ICP备19025352号-1</a> -->
 </div>
 </center>
